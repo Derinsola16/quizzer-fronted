6f0d5e03e554a438ba56d6c9e233d2ed37fd9682
derinsola16 <aderinsolaodusnay@gmail.com>
2019-08-21 09:11:14 +0100

    Created the Authentication pages and currently working on creating the question page

---
+++ src/views/Auth/Register.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2>Register</h2>
+        <form>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" v-model="user.email" required="" name="email" class="form-control" />
+                
+            </div>
+           <div class="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" v-model="user.password" required="" name="password" class="form-control" />
+                
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-primary" >Register</button> 
+                
+                <p> Already have an Account?<router-link to="/login" class="btn btn-link">Login</router-link></p>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+//import { mapState, mapActions } from 'vuex'
+export default {
+    name: 'register',
+
+    data: function (){
+        return{
+            user:{ },
+          
+        }
+    },
+    
+    
+}
+</script>
+
+<style scoped>
+.form-control{
+     width: 650px; 
+    margin-left: 250px;
+ 
+}
+</style>
---
+++ src/views/SetQuiz.vue
@@ -0,0 +1,51 @@
+<template>
+<main>
+
+    <form>
+        <div class="form-group">
+        <label for="course"> Enter Course &nbsp;</label>
+        <input type="text" required="" name="course"/>
+        </div>
+
+        <div class="form-group">
+        <label for="course"> Enter Question &nbsp;</label>
+        <input type="text" required="" name="question" placeholder="Description"/>
+        </div>
+
+        <div class="form-group">
+        <label for="course"> Enter Option 1 &nbsp;</label>
+        <input type="text" required="" name="course"/>
+        </div>
+
+        <div class="form-group">
+        <label for="course"> Enter Option 2 &nbsp;</label>
+        <input type="text" required="" name="course"/>
+        </div>
+
+        <div class="form-group">
+        <label for="course"> Enter Option 3 &nbsp;</label>
+        <input type="text" required="" name="course"/>
+        </div>
+
+        <div class="form-group">
+        <label for="course"> Enter Option 4 &nbsp;</label>
+        <input type="text" required="" name="course"/>
+        </div>
+    </form>
+</main>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.form-control{
+     width: 650px; 
+    margin-left: 250px;
+ 
+}
+</style>
